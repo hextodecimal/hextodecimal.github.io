d7ed05630031c5ebf7bcd4c424082a5d9d2eecca
Mert Şamil Gül <mertsamilgul@gmail.com>
2021-03-11 09:55:23 +0300

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+function convert_hex_to_dec(hex){
+    if(hex == "")
+        hex = "0";
+
+    hex = "0x" + hex;
+    let decimal = BigInt(hex);
+    let result = document.getElementById("result-dec");
+    result.innerText = decimal;
+
+    document.getElementById("dec").value = decimal;
+}
+
+function convert_dec_to_hex(decimal){
+    if(decimal == "")
+        decimal = "0";
+
+    decimal = BigInt(decimal);
+    let hex = decimal.toString(16).toUpperCase();
+    let result = document.getElementById("result-hex");
+    result.innerText = "0x" + hex;
+
+    document.getElementById("hex").value = hex;
+}
+
+function copy_to_clipboard(result_div){
+    var textToCopy = document.getElementById("result-" + result_div).innerText;
+
+    if(textToCopy[1] === "x") 
+        textToCopy = textToCopy.substring(2);
+
+    var textArea = document.createElement("textarea");
+    textArea.value = textToCopy;
+
+    //alert(textToCopy + " copied to the clipboard!");
+    pop_up("pop-" + result_div);
+
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+}
+
+function pop_up(pop_id) {
+    var popup = document.getElementById(pop_id);
+    popup.classList.toggle("show");
+}
+
+
